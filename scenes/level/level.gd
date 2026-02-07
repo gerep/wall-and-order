6be4f04e5698835bab79a90e805c869e07d0b280
gerep <181501+gerep@users.noreply.github.com>
2026-02-07 20:52:08 -0300

    place towers randomly on the map

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -6,6 +6,7 @@ extends Node2D
 
 const WALKABLE_TILE: Vector2i = Vector2i(0, 0)
 
+const TOWER_SCENE = preload("uid://ckod6a185q6fh")
 const enemies: Array = [preload("uid://drgu2o61e8iki"), preload("uid://cjxa2ahc3d6py")]
 
 var screen_width: float
@@ -16,6 +17,7 @@ func _ready() -> void:
 	screen_width = viewport.size.x
 	enemy_spawn_timer.timeout.connect(_spawn_enemy)
 	_spawn_enemy()
+	_place_towers(3)
 
 	GameManager.tile_destroyed.connect(_on_tile_destroyed)
 	GameManager.game_ended.connect(_on_game_ended)
@@ -32,6 +34,29 @@ func _spawn_enemy() -> void:
 	enemy.tilemap_layer = ground_layer
 	add_child(enemy)
 	enemy.position = Vector2(spawn_position, -50.0)
+
+
+func _place_towers(count: int, min_distance: float = 150.0) -> void:
+	var cells = ground_layer.get_used_cells()
+	cells.shuffle()
+	var placed_positions: Array[Vector2] = []
+	for cell in cells:
+		if placed_positions.size() >= count:
+			break
+
+		var pos = ground_layer.map_to_local(cell)
+		var too_close = false
+		for placed in placed_positions:
+			if pos.distance_to(placed) < min_distance:
+				too_close = true
+				break
+		if too_close:
+			continue
+
+		var tower = TOWER_SCENE.instantiate()
+		add_child(tower)
+		tower.position = pos
+		placed_positions.append(pos)
 
 
 func _on_game_ended() -> void:

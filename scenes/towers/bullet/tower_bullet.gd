ad30efb75b9bb1559f53f935e608253f09af94d7
gerep <181501+gerep@users.noreply.github.com>
2026-02-07 13:52:37 -0300

    tower shoots tanks

--- scenes/towers/bullet/tower_bullet.gd
+++ scenes/towers/bullet/tower_bullet.gd
@@ -3,16 +3,25 @@ class_name TowerBullet
 
 var target_position: Vector2
 var speed: int = 300
+var direction: Vector2
+var life_time: float
 
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 
-func _on_body_entered(_body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
+	body.queue_free()
 	queue_free()
 
 
 func _process(delta: float) -> void:
-	var direction = (target_position - position).normalized()
+	life_time += delta
+	if life_time >= 2:
+		call_deferred(&"queue_free")
+		return
+
+	if direction == Vector2.ZERO:
+		direction = (target_position - position).normalized()
 	position += direction * speed * delta
--- scenes/towers/tower.gd
+++ scenes/towers/tower.gd
@@ -1,16 +1,37 @@
 extends StaticBody2D
+class_name Tower
 
 @onready var area2D: Area2D = $Area2D
+@onready var shooting_timer: Timer = $ShootingTimer
 
 const BULLET_SCENE = preload("uid://ce2yywdbrmmiv")
+
+var targets: Array[Node2D]
 
 
 func _ready() -> void:
 	area2D.body_entered.connect(_on_body_entered)
+	area2D.body_exited.connect(_on_body_exited)
+	shooting_timer.timeout.connect(_on_shooting_timer_timeout)
+
+
+func _shoot() -> void:
+	if targets.is_empty():
+		return
+
+	var bullet_scene: TowerBullet = BULLET_SCENE.instantiate()
+	bullet_scene.target_position = targets[0].global_position
+	get_parent().call_deferred("add_child", bullet_scene)
+	bullet_scene.position = position
 
 
 func _on_body_entered(body: Node2D) -> void:
-	var bullet_scene: TowerBullet = BULLET_SCENE.instantiate()
-	bullet_scene.target_position = body.position
-	get_parent().call_deferred("add_child", bullet_scene)
-	bullet_scene.position = position
+	targets.push_back(body)
+
+
+func _on_body_exited(body: Node2D) -> void:
+	targets.erase(body)
+
+
+func _on_shooting_timer_timeout() -> void:
+	_shoot()

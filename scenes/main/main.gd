b7bb8aacb1283d59e1676d2938d243ef67e40195
gerep <181501+gerep@users.noreply.github.com>
2026-02-09 20:54:46 -0300

    add theme

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -5,7 +5,17 @@ extends Node2D
 
 func _ready() -> void:
 	play_button.pressed.connect(_on_play_button_pressed)
+	play_button.mouse_entered.connect(_on_mouse_entered)
+	play_button.mouse_exited.connect(_on_mouse_exited)
 
 
 func _on_play_button_pressed() -> void:
 	GameManager.go_to_level()
+
+
+func _on_mouse_entered() -> void:
+	play_button.rotation = deg_to_rad([-2.0, 2.0].pick_random())
+
+
+func _on_mouse_exited() -> void:
+	play_button.rotation = 0.0
--- scenes/ui/gameover.gd
+++ scenes/ui/gameover.gd
@@ -8,6 +8,10 @@ func _ready() -> void:
 	pass
 	try_again_button.pressed.connect(_on_try_again_pressed)
 	back_to_menu.pressed.connect(_on_back_to_menu_pressed)
+	back_to_menu.mouse_entered.connect(_on_back_to_menu_mouse_entered)
+	back_to_menu.mouse_exited.connect(_on_back_to_menu_mouse_exited)
+	try_again_button.mouse_entered.connect(_on_try_again_button_mouse_entered)
+	try_again_button.mouse_exited.connect(_on_try_again_button_mouse_exited)
 
 
 func _on_try_again_pressed() -> void:
@@ -16,3 +20,23 @@ func _on_try_again_pressed() -> void:
 
 func _on_back_to_menu_pressed() -> void:
 	GameManager.go_to_main_menu()
+
+
+func _on_play_button_pressed() -> void:
+	GameManager.go_to_level()
+
+
+func _on_back_to_menu_mouse_entered() -> void:
+	back_to_menu.rotation = deg_to_rad([-2.0, 2.0].pick_random())
+
+
+func _on_back_to_menu_mouse_exited() -> void:
+	back_to_menu.rotation = 0.0
+
+
+func _on_try_again_button_mouse_entered() -> void:
+	try_again_button.rotation = deg_to_rad([-2.0, 2.0].pick_random())
+
+
+func _on_try_again_button_mouse_exited() -> void:
+	try_again_button.rotation = 0.0
